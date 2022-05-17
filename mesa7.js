de5f14afd1021a89ce2828fb8a947d14d955b088
Felipe <felip120364@gmail.com>
2022-05-17 18:22:16 -0300

    atividade feita

---
+++ mesa7.js
@@ -0,0 +1,58 @@
+let Status = "true";
+
+if (Status == "true"){
+console.log("O valor é verdadeiro");
+}
+
+else{
+    console.log("O valor é falso");
+}
+
+let Linguagem = "javascript";
+
+if (Linguagem == "javascript"){
+console.log("Estou aprendendo!");
+}
+
+else{
+    console.log("Estou tentando!");
+}
+
+// exp1
+console.log ( !true );
+
+// exp2
+console.log ( !false );
+
+// exp3
+console.log ( !!false, true );
+
+// exp4
+console.log ( !!true );
+
+// exp5
+console.log ( !1 );
+
+// exp6
+console.log ( !!1 );
+
+// exp7
+console.log ( !0 );
+
+// exp8
+console.log ( !!0 );
+
+// exp9
+console.log ( !!"" );
+
+// exp10
+let x = 5 ;
+
+let y = 9;
+if (x < 10 && x!==5){
+    console.log( true )
+}
+else if(x>9 || x===5){
+    console.log( true )
+}
+else !(x===y)
